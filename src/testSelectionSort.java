bc3e1165216168238d20a8e18a6d78748936a74c
Byron-Hodel <byron@hodel.com>
2022-12-01 17:06:06 -0500

    implimented test cases

--- src/testSelectionSort.java
+++ src/testSelectionSort.java
@@ -29,16 +29,53 @@ public class testSelectionSort {
 
 		/** add tests to check for this unit test **/
 	}
-	
+
 	public void testNegative(){
 		/** Test data contains negative values only **/
 	}
-	
+
 	public void testMixed(){
-		/** Test data contains with both positive, negative and zeros **/
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = 9;
+		arr[2] = -7;
+		arr[3] = 10;
+		arr[4] = -2;
+		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -8;
+		Sortedarr[1] = -7;
+		Sortedarr[2] = -2;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = 10;
+		SelectionSort selection_sort = new SelectionSort();
+		arr = selection_sort.basicSelectionSort(arr);
+		
+		for(int i = 1; i < Sortedarr.length; i++) {
+			assert(arr[i] == Sortedarr[i]);
+		}
 	}
 	
-	public void testDuplicates(){
-		/** Test data contains duplicates **/
+	public void testDuplicates() {
+		int[] arr = new int[5];
+		arr[0] = 3;
+		arr[1] = 1;
+		arr[2] = 2;
+		arr[3] = 1;
+		arr[4] = 3;
+		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 1;
+		Sortedarr[1] = 1;
+		Sortedarr[2] = 2;
+		Sortedarr[3] = 3;
+		Sortedarr[4] = 3;
+		SelectionSort selection_sort = new SelectionSort();
+		arr = selection_sort.basicSelectionSort(arr);
+		
+		for(int i = 1; i < Sortedarr.length; i++) {
+			assert(arr[i] == Sortedarr[i]);
+		}
 	}
+
 }
